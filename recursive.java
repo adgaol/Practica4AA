575d910f7953ee5d7d6acfe897545fda16564494
DLois97 <37405525+DLois97@users.noreply.github.com>
2018-11-13 22:04:50 +0100

    Add files via upload

--- recursive.java
+++ recursive.java
@@ -1,4 +1,4 @@
-package practica4AA;
+
 
 public class recursive {
 	public static int f (int x, int y) {
@@ -18,7 +18,7 @@ public class recursive {
 		}
 		frecT(x, y, tabla);
 		return tabla[x][y];
-		} 
+		}  
 	
 	public static void frecT (int x, int y, int[][] tabla) {
 		if(tabla[x][y]==-1) {
@@ -33,6 +33,22 @@ public class recursive {
 				tabla[x][y]=tabla[x][y-1]+ tabla[x-1][y] +tabla[x-1][y-1] ;
 			}
 		} 
+	}
+	public static int fTab(int x, int y) {
+		int [][] tab = new int[x+1][y+1];
+		for (int i = 0; i < x+1;i++) {
+			tab[i][0] = i;
+		}
+		for (int i = 0; i < y+1;i++) {
+			tab[0][i] = i;
+		}
+		for(int i=1;i<x+1;i++) {
+			for(int j=1;j<y+1;j++) {
+				tab[i][j]=tab[i][j-1]+tab[i-1][j]+tab[i-1][j-1];
+			}
+		}
+		return tab[x][y];
+		
 	}
 
 }
